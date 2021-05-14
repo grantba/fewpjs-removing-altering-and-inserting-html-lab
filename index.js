279ce370a3025a4bd1bf4341dc1c9e899ccff29b
Blaire <grant.blaire@gmail.com>
2021-05-14 15:25:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// no longer has DOM node 'main#main': Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+main.remove();
+// has a 'newHeader' variable that points to node 'h1#victory': newHeader is not defined
+let newHeader = document.createElement("h1");
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside: newHeader is not defined
+newHeader.id = "victory";
+newHeader.innerHTML = "Blaire is the champion";
